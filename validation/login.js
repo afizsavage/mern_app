7103be5e388061502541c4e656fca38c8451a112
Afiz Savage <savageafiz9@gmail.com>
2020-06-22 21:28:08 +0000

    backend infrastructure

---
+++ validation/login.js
@@ -0,0 +1,24 @@
+const validator = require("validator");
+const isEmpty = require("is-empty");
+
+module.exports = function validateLoginInput(data) {
+  let errors = {};
+
+  //Convert empty fields to empty string so we can use validator functions
+  data.email = !isEmpty(data.email) ? data.email : "";
+  data.password = !isEmpty(data.password) ? data.password : "";
+
+  //Email checks
+  if (validator.isEmpty(data.email)) {
+    errors.email = "Email is required";
+  } else if (!validator.isEmail(data.email)) {
+    errors.email = "Email is invalid";
+  }
+
+  //Password checks
+  if (validator.isEmpty(data.password)) {
+    errors.password = "Password field is required";
+  }
+
+  return { errors, isValid: isEmpty(errors) };
+};
